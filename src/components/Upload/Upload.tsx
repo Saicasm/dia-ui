d4f26ac68aa3e4a45724b9ad6c13a7a651173c74
Sai <sai98nsk@gmail.com>
2024-07-01 11:58:02 -0400

    fix: added basic navigation panel

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -13,25 +13,40 @@ interface ButtonProps {
    * Additional classes to be added to the button
    */
   className?: string;
-  name?:string
+  name?: string;
+  subtext?: string;
 }
 
 const Upload: React.FC<ButtonProps> = ({
-onFileChange,
+  onFileChange,
   children,
   className = "",
-  name= "Upload"
+  name = "Upload",
+  subtext = "",
 }) => {
   let buttonClasses = "px-4 py-2 rounded focus:outline-none ";
 
   return (
-<div>
-
-<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">{children}</label>
-<input className="rounded-lg block bg-light-bg-primary text-light-text-secondary px-4 py-2 " onChange={onFileChange} id="file_input" type="file"/>
-<p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
-</div>
-
+    <div>
+      <label
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="file_input"
+      >
+        {children}
+      </label>
+      <input
+        className="rounded-lg block bg-light-bg-primary text-light-text-secondary px-4 py-2 "
+        onChange={onFileChange}
+        id="file_input"
+        type="file"
+      />
+      <p
+        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+        id="file_input_help"
+      >
+        {subtext}
+      </p>
+    </div>
   );
 };
 
