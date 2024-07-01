675753e6d22c40a4ce56ef5d8865cc0f5343f8cf
Sai <sai98nsk@gmail.com>
2024-07-01 17:54:05 -0400

    feat: added secondary variant for upload

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,6 +1,7 @@
-import { Copy, House, Search } from "lucide-react";
+import { Copy, House, Search, Upload } from "lucide-react";
 export const Icons = {
   copy: Copy,
   search: Search,
   home: House,
+  upload: Upload,
 };
--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icons } from "../Icons/Icons";
 
 interface ButtonProps {
   /**
@@ -15,6 +16,7 @@ interface ButtonProps {
   className?: string;
   name?: string;
   subtext?: string;
+  variant: string;
 }
 
 const Upload: React.FC<ButtonProps> = ({
@@ -23,29 +25,59 @@ const Upload: React.FC<ButtonProps> = ({
   className = "",
   name = "Upload",
   subtext = "",
+  variant = "primary",
 }) => {
-  let buttonClasses = "px-4 py-2 rounded focus:outline-none ";
-
+  function PrimaryVariant() {
+    return (
+      <>
+        <label
+          className="block mb-2 text-base font-medium text-light-text-primary "
+          htmlFor="file_input"
+        >
+          {children}
+        </label>
+        <input
+          className="rounded-lg   text-light-text-primary mx-4 py-2"
+          onChange={onFileChange}
+          id="file_input"
+          type="file"
+          style={{ background: "bg-light-bg-secondary" }}
+        />
+        <p
+          className="mt-1 text-sm text-light-text-primary dark:text-gray-300"
+          id="file_input_help"
+        >
+          {subtext}
+        </p>
+      </>
+    );
+  }
+  function SecondaryVariant() {
+    return (
+      <>
+        {" "}
+        <div className="flex items-center justify-center w-full">
+          <label
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed text-light-text-primary rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          >
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <Icons.upload />
+              <p className="mb-2 text-sm ">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-xs ">{subtext}</p>
+            </div>
+            <input id="dropzone-file" type="file" className="hidden" />
+          </label>
+        </div>
+      </>
+    );
+  }
   return (
-    <div>
-      <label
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        htmlFor="file_input"
-      >
-        {children}
-      </label>
-      <input
-        className="rounded-lg block bg-light-bg-primary text-light-text-secondary px-4 py-2 "
-        onChange={onFileChange}
-        id="file_input"
-        type="file"
-      />
-      <p
-        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-        id="file_input_help"
-      >
-        {subtext}
-      </p>
+    <div className="mx-4 my-2 dark:text-dark-bg-primary">
+      {variant == "primary" ? <PrimaryVariant /> : <SecondaryVariant />}
     </div>
   );
 };
