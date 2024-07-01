31df3a256a69acb5fe57fd71df9d56c20d5bcc1c
Sai <sai98nsk@gmail.com>
2024-07-01 10:52:32 -0400

    feat: added sidenavbar and upload component

---
+++ src/components/Upload/Upload.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface ButtonProps {
+  /**
+   * Optional click handler
+   */
+  onFileChange?: (file: any) => void;
+  /**
+   * The content of the button
+   */
+  children?: React.ReactNode;
+  /**
+   * Additional classes to be added to the button
+   */
+  className?: string;
+  name?:string
+}
+
+const Upload: React.FC<ButtonProps> = ({
+onFileChange,
+  children,
+  className = "",
+  name= "Upload"
+}) => {
+  let buttonClasses = "px-4 py-2 rounded focus:outline-none ";
+
+  return (
+<div>
+
+<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">{children}</label>
+<input className="rounded-lg block bg-light-bg-primary text-light-text-secondary px-4 py-2 " onChange={onFileChange} id="file_input" type="file"/>
+<p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+</div>
+
+  );
+};
+
+export default Upload;
