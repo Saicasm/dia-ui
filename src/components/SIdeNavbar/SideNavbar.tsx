74920524422f18f2836bc0ce5812252677c24c02
Sai <sai98nsk@gmail.com>
2024-07-01 12:16:01 -0400

    feat: added icons to the project

--- src/components/SIdeNavbar/SideNavbar.tsx
+++ src/components/SIdeNavbar/SideNavbar.tsx
@@ -3,20 +3,10 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
-const sections = [
-  { title: "Home", path: "/" },
-  { title: "About", path: "/" },
-  { title: "Contact", path: "/" },
-];
+import { Icons } from "@/components/Icons/Icons";
 
 const Sidenavbar = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const router = useRouter();
-
-  const toggleTheme = () => {
-    setIsDarkTheme((prev) => !prev);
-  };
 
   return (
     <div
@@ -37,21 +27,8 @@ const Sidenavbar = () => {
               data-collapse-toggle="dropdown-pages"
               onClick={() => router.push("/home")}
             >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <Icons.home />
               <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                {/* <Link href="/">Home</Link> */}
                 Home
               </span>
             </button>
@@ -64,19 +41,7 @@ const Sidenavbar = () => {
               data-collapse-toggle="dropdown-sales"
               onClick={() => router.push("/search")}
             >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <Icons.search />
               <span className="flex-1 ml-3 text-left whitespace-nowrap">
                 Search
               </span>
