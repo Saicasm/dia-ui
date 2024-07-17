94545623e0005f55f4d75553372988eefe22cb64
Sai <sai98nsk@gmail.com>
2024-07-17 18:44:32 -0500

    fix: added active status for routes

--- src/components/SIdeNavbar/SideNavbar.tsx
+++ src/components/SIdeNavbar/SideNavbar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { appRoute } from "@/config/routing/app-route";
 const Sidenavbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <div className="top-0 left-0 z-40 w-48 h-full transition-transform -translate-x-full sm:translate-x-0 ">
@@ -13,18 +14,27 @@ const Sidenavbar = () => {
           <div className="text-start h-12 font-bold">Diaconia</div>
         </div>
         <div className="flex flex-col my-4 justify-center align-middle mx-4">
-          {appRoute?.map((item, index) => (
-            <div
-              className="flex  justify-start items-center h-12  hover:cursor-pointer w-full text-base font-normal text-light-text-primary  rounded-lg transition duration-75 group  dark:bg-dark-text-primary "
-              onClick={() => router.push(item.routePath)}
-              key={index}
-            >
-              <div className=" mx-2">{item.icon}</div>
-              <span className="  text-center whitespace-nowrap">
-                {item.name}
-              </span>
-            </div>
-          ))}
+          {appRoute?.map((item, index) => {
+            const isActive = pathname === `/${item.routePath}`;
+            console.log(pathname);
+            console.log(isActive);
+            return (
+              <div
+                className={`flex justify-start items-center h-12 hover:cursor-pointer w-full text-base font-normal rounded-lg transition duration-75 group ${
+                  isActive
+                    ? "bg-light-accent-primary text-light-text-primary font-semibold dark:bg-dark-accent-primary dark:text-dark-text-primary"
+                    : "text-light-text-primary dark:text-dark-text-primary hover:bg-light-bg-hover dark:hover:bg-dark-bg-hover"
+                }`}
+                onClick={() => router.push(item.routePath)}
+                key={index}
+              >
+                <div className="mx-2">{item.icon}</div>
+                <span className="text-center whitespace-nowrap">
+                  {item.name}
+                </span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Button from "@/components/Button/Button";
 import { useState } from "react";
-import Upload from "@/components/Upload/Upload";
 import RootLayout from "@/app/layout";
 
 export default function Home() {
@@ -16,9 +14,18 @@ export default function Home() {
   };
   return (
     <RootLayout>
-      <main className="flex  shadow-md flex-col items-center justify-between 	 bg-light-bg-primary bg-opacity-90">
-        <div className="flex flex-col">Settings</div>
-        <div></div>
+      <main className="flex min-h-screen flex-col items-center justify-between 	 bg-light-bg-primary bg-opacity-90">
+        <div className="flex shadow-md  flex-col items-center justify-between	w-full ">
+          <div className="flex flex-col justify-evenly w-full ">
+            <div className=" border-b-2 font-mono m-4">Settings</div>
+          </div>
+          <div className="m-4">
+            <div className=" h-10 font-semibold">Select Analysis Type</div>
+          </div>
+          <div className="m-4">
+            <div className=" h-10 font-semibold">Change default prompts</div>
+          </div>{" "}
+        </div>
       </main>
     </RootLayout>
   );
