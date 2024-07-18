fc3bd6225997550891add1325b8fb9605248c9d9
Sai <sai98nsk@gmail.com>
2024-07-17 19:42:21 -0500

    test: added unit test for sidenavbar

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const nextJest = require("next/jest");
-
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
@@ -9,6 +10,9 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
