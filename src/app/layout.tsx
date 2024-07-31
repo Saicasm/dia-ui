1ad3b0f8053d776e9847b01053bb6d56d5e4c3bd
Sai <sai98nsk@gmail.com>
2024-07-31 17:33:44 -0400

    hotfix: disabled settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,11 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen mx-auto  bg-light-bg-primary dark:bg-dark-bg-primary flex flex-col md:flex-row ">
-      <div className="md:hidden">
+    <div className="min-h-screen mx-auto  bg-light-bg-primary dark:bg-dark-bg-primary flex flex-col md:flex-row bg-custom-gradient">
+      <div className="md:hidden bg-custom-gradient">
         <Header title={"Diaconia"} />
       </div>
-      <div className="hidden md:block ">
+      <div className="hidden md:block bg-custom-gradient ">
         <Sidenavbar />
       </div>
       <main className="max-h-full flex-grow pr-12 pl-12 md:pl-24 md:pr-24 md:py-10 ">
--- src/components/SIdeNavbar/SideNavbar.tsx
+++ src/components/SIdeNavbar/SideNavbar.tsx
@@ -9,21 +9,25 @@ const Sidenavbar = () => {
 
   return (
     <div className="top-0 left-0 z-40 w-48 h-full transition-transform -translate-x-full sm:translate-x-0 ">
-      <div className="overflow-y-auto py-5 px-3 h-full bg-light-bg-primary border-r border-light-border-primary dark:bg-dark-bg-primary dark:border-dark-border-primary flex flex-col justify-start">
-        <div className="flex  border-b-2 m-4 justify-center align-middle border-light-border-primary">
+      <div className="overflow-y-auto py-5 px-3 h-full bg-custom-gradient border-r border-black dark:bg-dark-bg-primary dark:border-dark-border-primary flex flex-col justify-start">
+        <div className="flex  border-b-2 m-4 justify-center align-middle border-black">
           <div className="text-start h-12 font-bold">Diaconia</div>
         </div>
         <div className="flex flex-col my-4 justify-center align-middle mx-4">
           {appRoute?.map((item, index) => {
             const isActive = pathname === `/${item.routePath}`;
+            const isDisabled = item.isDisabled === true; // Ensure it's a boolean
+
             return (
               <div
-                className={`flex justify-start items-center h-12 hover:cursor-pointer w-full text-base font-normal rounded-lg transition duration-75 group ${
+                className={`flex justify-start items-center h-12  w-full text-base font-normal rounded-lg transition duration-75 group ${
                   isActive
                     ? "bg-light-accent-primary text-light-text-primary font-semibold dark:bg-dark-accent-primary dark:text-dark-text-primary"
-                    : "text-light-text-primary dark:text-dark-text-primary hover:bg-light-bg-hover dark:hover:bg-dark-bg-hover"
+                    : isDisabled
+                      ? "cursor-not-allowed	"
+                      : "text-white dark:text-dark-text-primary hover:bg-light-bg-hover dark:hover:bg-dark-bg-hover hover:cursor-pointer cursor-pointer"
                 }`}
-                onClick={() => router.push(item.routePath)}
+                onClick={() => !isDisabled && router.push(item.routePath)}
                 key={index}
               >
                 <div className="mx-2">{item.icon}</div>
